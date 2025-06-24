caaf8e97737bcc8e0507997f4ea45882f75ad559
Guillaume <gu.gorvel@distrame.fr>
2025-06-24 09:31:13 +0200

    feat(Report.php) add lat and lng properties

--- src/Entity/Report.php
+++ src/Entity/Report.php
@@ -63,6 +63,14 @@ class Report
     #[Groups(['report:read', 'report:write'])]
     private ?string $location = null;
 
+    #[ORM\Column]
+    #[Groups(['report:read', 'report:write'])]
+    private ?float $lat = null;
+
+    #[ORM\Column]
+    #[Groups(['report:read', 'report:write'])]
+    private ?float $lng = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -126,5 +134,25 @@ class Report
         $this->location = $location;
 
         return $this;
+    }
+
+    public function getLng(): ?float
+    {
+        return $this->lng;
+    }
+
+    public function setLng(?float $lng): void
+    {
+        $this->lng = $lng;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(?float $lat): void
+    {
+        $this->lat = $lat;
     }
 }
