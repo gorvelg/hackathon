b0731209d7330614ebd8e6566ed73ff8c9ebbedb
Guillaume <gu.gorvel@distrame.fr>
2025-06-26 08:31:03 +0200

    feat: add post

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -88,11 +88,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(targetEntity: Report::class, mappedBy: 'author')]
     private Collection $reports;
 
+    /**
+     * @var Collection<int, Post>
+     */
+    #[ORM\OneToMany(targetEntity: Post::class, mappedBy: 'author', orphanRemoval: true)]
+    private Collection $posts;
+
     public function __construct()
     {
         $this->uid = Uuid::v4()->toRfc4122(); // ← string, 36 caractères, format UUID standard
         $this->contents = new ArrayCollection();
         $this->reports = new ArrayCollection();
+        $this->posts = new ArrayCollection();
     }
 
     public function getUid(): ?string
@@ -199,6 +206,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             // set the owning side to null (unless already changed)
             if ($report->getAuthor() === $this) {
                 $report->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getAuthor() === $this) {
+                $post->setAuthor(null);
             }
         }
 
