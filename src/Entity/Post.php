8156e5cba95a8a63f46b4f79add2e7e69a664769
Guillaume <gu.gorvel@distrame.fr>
2025-06-26 09:25:41 +0200

    fix(Post.php) conflict between name Post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post as PostOperation;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
@@ -19,8 +20,8 @@ use Symfony\Component\Uid\Uuid;
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ApiResource(
     operations: [
-        new \ApiPlatform\Metadata\Post(processor: PostProcessor::class),
         new GetCollection(),
+        new PostOperation(processor: PostProcessor::class),
         new Get(uriTemplate: '/contents/{uid}', uriVariables: ['uid']),
         new Put(uriTemplate: '/contents/{uid}', uriVariables: ['uid']),
         new Patch(
